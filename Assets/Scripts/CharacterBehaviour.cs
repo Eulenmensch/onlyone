f303257d931bed5e809dfa4a5c39c394397e2328
Yoshua Woo <yoshua.woo@gmail.com>
2019-08-04 14:40:46 +0200

    very much fixing since the last push broke a bunch of stuff. I now know again, what the code is doing since learning about local dotweens.

--- Assets/Scripts/CharacterBehaviour.cs
+++ Assets/Scripts/CharacterBehaviour.cs
@@ -8,17 +8,25 @@ public class CharacterBehaviour : MonoBehaviour
     public float FlingHeight;
     public float FlingForce;
     public float Threshold;
-    public bool Airborne;
-    public bool DoubleJumped;
+    private bool Airborne;
+    private bool DoubleJumped;
     public GameObject Platform;
 
     private PlatformTopController PlatTopCont;
     private Rigidbody2D Rigidbody;
 
+
+    public float DoubleJumpForce;
+    public float Speed;
+    private Animator CharAnimator;
+    private CharacterBehaviour CharBehaviour;
+
+
     // Start is called before the first frame update
     void Start()
     {
         PlatTopCont = Platform.GetComponent<PlatformTopController>();
+        CharAnimator = GetComponent<Animator>();
         Rigidbody = GetComponent<Rigidbody2D>();
     }
 
@@ -26,15 +34,19 @@ public class CharacterBehaviour : MonoBehaviour
     void Update()
     {
         Fling();
+        Move();
+        DoubleJump();
+        FlipSprite();
     }
 
     void Fling()
     {
         var isGrounded = GetGroundedOnPlatform();
-        if (PlatTopCont.StretchHeight - Platform.transform.position.y <= Threshold && isGrounded)
+        if (PlatTopCont.StretchHeight - Platform.transform.localPosition.y <= Threshold && isGrounded)
         {
             print("fling!");
             Airborne = true;
+            CharAnimator.SetBool("Airborne", true);
             GetComponent<SpringJoint2D>().enabled = false;
             var xInput = Input.GetAxis("Horizontal");
             var flingDir = new Vector2(xInput, FlingHeight);
@@ -57,12 +69,60 @@ public class CharacterBehaviour : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject == Platform)
+        var contact = new ContactPoint2D();
+        contact = other.GetContact(0);
+        if (contact.normal == Vector2.up)
         {
             print("collided");
-            GetComponent<SpringJoint2D>().enabled = true;
             Airborne = false;
+            CharAnimator.SetBool("Airborne", false);
             DoubleJumped = false;
+            if (other.gameObject == Platform)
+            {
+                GetComponent<SpringJoint2D>().enabled = true;
+            }
+        }
+    }
+
+    void Move()
+    {
+        var xInput = Input.GetAxisRaw("CharacterHorizontal");
+        if (xInput != 0)
+        {
+            var xVel = xInput * Speed * Time.deltaTime;
+            Rigidbody.velocity = new Vector2(xVel, Rigidbody.velocity.y);
+            CharAnimator.SetBool("Walking", true);
+        }
+        // else if (!Airborne)
+        // {
+        //     Rigidbody.velocity = new Vector2(0, Rigidbody.velocity.y);
+        //     CharAnimator.SetBool("Walking", false);
+        // }
+        else
+        {
+            CharAnimator.SetBool("Walking", false);
+        }
+    }
+
+    void DoubleJump()
+    {
+        if (Input.GetButtonDown("Jump") & !DoubleJumped)
+        {
+            Rigidbody.AddForce(Vector2.up * DoubleJumpForce, ForceMode2D.Impulse);
+            DoubleJumped = true;
+        }
+    }
+
+    void FlipSprite()
+    {
+        var xInput = Input.GetAxisRaw("CharacterHorizontal");
+        if (xInput < 0)
+        {
+            transform.localScale = new Vector2(-1, 1);
+        }
+        if (xInput > 0)
+        {
+            transform.localScale = new Vector2(1, 1);
         }
     }
 }
